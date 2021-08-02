e58a23ec5cda3567619d40d897887f655b1cc1ae
kylin007 <1052150650@qq.com>
2021-08-02 14:24:09 +0800

    Update main.java

--- src/main/java/cn/kylin/leetcode/main.java
+++ src/main/java/cn/kylin/leetcode/main.java
@@ -5,4 +5,13 @@ package cn.kylin.leetcode;
  * @Date: 2021/1/18 下午5:57
  */
 public class main {
+  
+  // 手动修改
+  
+      public static void main(String[] args) {
+        System.out.println("sss");
+        System.out.println("mmm");
+        System.out.println("qqq");
+    }
+  
 }
